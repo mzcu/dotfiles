fbb16282b3faa74c63b34aefaef7831324186cfd
Milos Cubrilo <milos.cubrilo@gmail.com>
2018-06-23 12:02:53 +0200

    Improve xmobar config

--- xmonad/xmobarrc.hs
+++ xmonad/xmobarrc.hs
@@ -5,15 +5,15 @@ Config { font = "xft:Ubuntu Mono:style=bold:pixelsize=14:antialias=true"
         , fgColor = "grey"
         , position = TopW L 100
         , commands = [
-                          Run Network "enp0s31f6" ["-L","0","-H","1024000", "--high","green", "-t", "NET: <rx>K/<tx>K"] 10
+                          Run DynNetwork ["--Low","0","--High","1024000", "--high","green", "--template", "<dev>: <rx>KBs/<tx>KBs"] 10
                         , Run Cpu ["-p", "3", "-L","0","-H","50","--normal","green","--high","red","-t", "CPU: <user>%u <system>%s <iowait>%i"] 10
                         , Run Memory ["-t","MEM: <used>M/<total>M"] 10
                         , Run Com ".xmonad/bin/volume" [] "vol" 10
-                        , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
+                        , Run Date "%T" "date" 10
                         , Run StdinReader
-		--	, Run Weather "EHAM" ["-t","<tempC>C","-L","18","-H","25","--normal","green","--high","red","--low","lightblue"] 36000
+			, Run Weather "EHAM" ["-t","<tempC>C <skyCondition> (<windCardinal> <windMs> m/s)","-L","18","-H","25","--normal","green","--high","red","--low","lightblue"] 36000
                         ]
         , sepChar = "%"
         , alignSep = "}{"
-        , template = "%StdinReader% }{ VOL: %vol% | %cpu% | %memory% | %enp0s31f6% | <fc=#ee9a00>%date%</fc> "
+        , template = "%StdinReader% }{ VOL: %vol% | %cpu% | %memory% | %dynnetwork% | %EHAM% | <fc=#ee9a00>%date%</fc>                "
         }
